030e3969c352a4082723660b83a3ebbe35c48b2e
adhunikFireXpert <info@adhunikfirexpert.com>
2025-11-28 10:53:50 +0530

    Update GlowSignEmergencyLight.js

--- src/app/glow-signage-&-emergency-lights/GlowSignEmergencyLight.js
+++ src/app/glow-signage-&-emergency-lights/GlowSignEmergencyLight.js
@@ -197,7 +197,7 @@ export default function GlowSignEmergencyLight() {
 
                     <img
                       className=" w-full h-full object-center rounded-3xl object-contain"
-                      src="https://images.unsplash.com/photo-1707234377854-69d1f130b4ff?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      src=https:"//res.cloudinary.com/djablkulv/image/upload/v1764307292/Glow_Signage_hy8h6q.webp"
                       alt="Glow Signage & Emergency Lights"
                       loading="lazy"
                     />
